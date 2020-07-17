22b2767b623d89fef93d35bd32ca5aedbbf7e127
jenenneihart <neihart.jenen@gmail.com>
2020-07-17 16:08:56 -0700

    going back and working on the game working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,55 @@
+$(document).ready(function (){
+    
 
-const fighterNames = [{
-    "name": "Walter",
-}, {
-    "name": "Mike"
-}, {
-    "name": "Jesse",
-}];
-$(document).ready(function() {
-showDialog("Welcome to Breaking Bad",
-        "Today you will face some challenging opponents." +
-        "Your objective is to fight each opponent until you are the victor or die trying " +
-        "Let's Play", 
-}
-function showDialog(title) {
-    let message = "<div id='dialog-message' title='" + title + "'><p>" + showDialog + "</p></div>"
-    $("#gameboard").append(message);
-    $("#dialog-message").dialog({
-            text: btnText,
-            click: function() {
-                action();
+    var characters = {
+        "Walter White": {
+            name: "Walter White",
+            health: 200,
+            attack: 6,
+            imageUrl: "assets/images/Walter.jpg",
+            enemyAttackBack: 12
+        },
+        "Jessie Pinkman": {
+            name: "Walter White",
+            health: 175,
+            attack: 9,
+            imageUrl: "assets/images/Jesse.jpg",
+            enemyAttackBack: 8
+        },
+        "Mike Ehrmantraut": {
+            name: "Mike Ehrmantraut",
+            health: 300,
+            attack: 5,
+            imageUrl: "assets/images/Mike.jpg",
+            enemyAttackBack: 15
+        }
 
-     function (showAttackButton).clickfunction() {
-        var attackButton = $("<button>").addClass("btn btn-attack");
-         attackButton.html("Attack!!")   },  
-        
-         assignAttacker function(Attacker) {
-            gameBattle.currentAttacker = attacker;
-            $("#" + gameBattle.currentAttacker._id).removeClass("fighter").addClass("Attacker");  },
+    };
 
-         assignDefender function(defender) {
-            gameBattle.currentDefender = defender;
-            $("#" + gameBattle.currentDefender._id).removeClass("fighter").addClass("defender");  },     
-         
+    var attacker;
+    var fighters = [];
+    var defender;
+    var turnCounter = 1;
+    var killCount = 0;
+
+
+    var renderCharacter = function (character, renderArea) {
+
+        var charDiv = $("<div class='chracter' data-name='" + character.name + "'>");
+        var charName = $("<div class='character-name'>").text(character.name);
+        var charImage = $("<img alt='image' class='character-image'>").attr("src", character.imageUrl);
+        var charHealth = $("<div class='character-health'>").text(character.health);
+        charDiv.append(charName).append(charImage).append(charHealth);
+        $(renderArea).append(charDiv);
+    };
+
+    var initializeGame = function() {
+        for (var key in characters) {
+          renderCharacter(characters[key], "#characters-section");
+        }
+      };
+
+
+
+
+    })
